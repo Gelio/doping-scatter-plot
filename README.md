dd22f5037533e7bf71da78848827f0e347c5b9de
Gelio <voreny.gelio@gmail.com>
2016-02-05 15:56:05 +0100

    initial commit, environment set up

---
+++ README.md
@@ -0,0 +1,3 @@
+# Bicycle racing scatter plot #
+
+A task from FreeCodeCamp to design and develop a scatter plot using D3.js that visualises 35 fastest times up Alpe d'Huez.
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    context: __dirname,
+    entry: [
+        './index.js'
+    ],
+    output: {
+        filename: 'bundle.js',
+        path: './public/'
+    },
+    devtool: 'inline-source-map',
+    module: {
+        loaders: [
+            {test: /\.css$/, loader: ExtractTextPlugin.extract(['css?sourceMap', 'autoprefixer'])},
+            {test: /\.scss$/, loader: ExtractTextPlugin.extract(['css-loader?sourceMap', 'autoprefixer', 'sass-loader?sourceMap']), exclude: /node_modules/},
+            {test: /\.html$/, loader: 'raw'}
+        ]
+    },
+    devServer: {
+        contentBase: './public'
+    },
+    plugins: [
+        new ExtractTextPlugin('bundle.css')
+    ]
+};
